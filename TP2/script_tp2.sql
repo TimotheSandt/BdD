b573e2e1adcad1b6b935b98c8bb0f0bf3532a529
TimotheSandt <timothe.sandt@edu.univ-fcomte.fr>
2025-02-12 10:07:28 +0100

    TP2 - R8 to R12

--- TP2/script_tp2.sql
+++ TP2/script_tp2.sql
@@ -67,7 +67,7 @@ LOAD DATA LOCAL INFILE 'COMPETITION.csv' INTO TABLE competition
 LOAD DATA LOCAL INFILE 'SKIEUR.csv' INTO TABLE skieur
     CHARACTER SET utf8 FIELDS TERMINATED BY ',';
 
-LOAD DATA LOCAL INFILE 'CLASSEMENT.csv' INTO TABLE classement
+LOAD DATA LOCAL INFILE 'classement.csv' INTO TABLE classement
     CHARACTER SET utf8 FIELDS TERMINATED BY ',';
 
 LOAD DATA LOCAL INFILE 'COMPORTE.csv' INTO TABLE comporte
@@ -137,3 +137,85 @@ WHERE station.nomStation = "Tignes" AND classement.classement = 1
 GROUP BY skieur.idSkieur, skieur.nomSkieur;
 
 
+-- R7
+
+
+
+-- Supprimer le contenu des tables comportant classement et competition
+DELETE FROM classement;
+DELETE FROM comporte;
+
+-- Insérer des enregistrements avec les fichiers CSV
+LOAD DATA LOCAL INFILE 'COMPORTEv2.csv' INTO TABLE comporte 
+    CHARACTER SET utf8 FIELDS TERMINATED BY ',';
+LOAD DATA LOCAL INFILE 'classementv2.csv' INTO TABLE classement 
+    CHARACTER SET utf8 FIELDS TERMINATED BY ',';
+
+-- Supprimer les enregistrements dans la table competition avec TRUNCATE
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE competition;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Insérer des enregistrements après TRUNCATE
+LOAD DATA LOCAL INFILE 'COMPETITIONv2.csv' INTO TABLE competition 
+    CHARACTER SET utf8 FIELDS TERMINATED BY ',';
+
+
+
+
+
+-- R8
+SELECT DISTINCT nomSkieur, classement.classement, stationCompet.nomStation AS lieu_compet, stationSkieur.nomStation AS club
+FROM skieur
+JOIN classement ON skieur.idSkieur = classement.idSkieur
+JOIN competition ON classement.idCompetition = competition.idCompetition
+JOIN station AS stationCompet ON competition.idStation = stationCompet.idStation
+JOIN station AS stationSkieur ON skieur.idStation = stationSkieur.idStation
+WHERE stationCompet.nomStation = 'Tignes' 
+AND skieur.idSkieur NOT IN (
+    SELECT idSkieur
+    FROM classement
+    WHERE classement >= 3
+);
+ 
+ 
+-- R9
+SELECT DISTINCT nomSkieur
+FROM skieur
+JOIN classement ON skieur.idSkieur = classement.idSkieur
+JOIN competition ON classement.idCompetition = competition.idCompetition
+JOIN station AS stationCompet ON competition.idStation = stationCompet.idStation
+JOIN station AS stationSkieur ON skieur.idStation = stationSkieur.idStation
+WHERE stationCompet.nomStation = stationSkieur.nomStation 
+AND skieur.idSkieur NOT IN (
+    SELECT idSkieur
+    FROM classement
+    WHERE classement >= 3
+);
+ 
+ 
+-- R10
+ALTER TABLE classement
+ADD COLUMN idSpecialite INT,
+ADD COLUMN temps TIME,
+ADD PRIMARY KEY (idSkieur, idCompetition, idSpecialite);
+ 
+SELECT * FROM classement;
+ 
+ 
+-- R11
+SELECT nomStation
+FROM station
+JOIN skieur ON station.idStation = skieur.idStation
+GROUP BY nomStation, station.idStation
+HAVING COUNT(skieur.idSkieur) >= 2;
+ 
+ 
+-- R12
+SELECT nomSkieur
+FROM skieur
+WHERE idSpecialite IN (
+    SELECT idSpecialite
+    FROM skieur
+    WHERE nomSkieur = 'paul'
+) AND NOT nomSkieur = 'paul';
